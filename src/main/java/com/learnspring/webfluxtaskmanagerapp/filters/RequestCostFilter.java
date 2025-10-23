9ae47bdbd95d946f363e316de1c6123a247c253e
maruti-panchal <panchal.maruti70@gmail.com>
2025-10-23 14:20:14 +0530

    created multiple filters and tried to modify their ordering and conditional base apply filtering.

--- src/main/java/com/learnspring/webfluxtaskmanagerapp/filters/RequestCostFilter.java
+++ src/main/java/com/learnspring/webfluxtaskmanagerapp/filters/RequestCostFilter.java
@@ -1,6 +1,7 @@
 package com.learnspring.webfluxtaskmanagerapp.filters;
 
 
+import org.springframework.core.annotation.Order;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
@@ -12,11 +13,17 @@ import java.time.Duration;
 import java.time.Instant;
 import java.util.logging.Logger;
 @Component
+@Order(1)
 public class RequestCostFilter implements WebFilter {
-    private static final Logger log = Logger.getLogger(RequestCostFilter.class.getName());
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
+        String path = exchange.getRequest().getURI().getPath();
+        if (path.equals("/auth/login") || path.equals("/auth/signup")) {
+            return chain.filter(exchange); // skip
+        }
+
+
         Instant start = Instant.now();
 
         ServerHttpResponse response = exchange.getResponse();
@@ -24,10 +31,12 @@ public class RequestCostFilter implements WebFilter {
         response.beforeCommit(() -> {
             long durationMs = Duration.between(start, Instant.now()).toMillis();
             response.getHeaders().set("X-REQUEST-COST", String.valueOf(durationMs)+"ms");
+            System.out.println("Cost Filter : Order 1");
             return Mono.empty();
         });
 
         // Continue the filter chain
+
         return chain.filter(exchange);
     }
 }
